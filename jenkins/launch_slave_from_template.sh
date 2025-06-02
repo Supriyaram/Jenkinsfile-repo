02083ff1a5312ead55e418c2990dc08c4df43b2b
Supriya R <supriyaram3498@gmail.com>
2025-06-02 20:13:28 +0530

    Create launch_slave_from_template.sh

---
+++ jenkins/launch_slave_from_template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LABEL=$1
+TEMPLATE_ID="lt-026fe4def668209ae"        # Update this
+SUBNET_ID="subnet-0659d444315ed93b5"          # Update this
+SECURITY_GROUP_ID="sg-0e752d73280a90ee5"      # Update this
+
+INSTANCE_ID=$(aws ec2 run-instances \
+  --launch-template "LaunchTemplateId=$TEMPLATE_ID,Version=1" \
+  --subnet-id "$SUBNET_ID" \
+  --security-group-ids "$SECURITY_GROUP_ID" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=jenkins-${LABEL}},{Key=jenkins-label,Value=${LABEL}}]" \
+  --query 'Instances[0].InstanceId' --output text)
+
+echo "$INSTANCE_ID" > slave_instance_id.txt
