5d9bfb19124b0536d68548ed31f7921108b9bfb6
Supriya <darshan01075@gail.com>
2025-06-03 19:42:05 +0530

    added docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,41 +1,85 @@
 @Library('dumbslave-lib') _
 
 pipeline {
-     agent { label 'built-in' }
+        agent { label 'built-in' }
+        parameters {
+                choice(name: 'REPO_SELECTION', choices: ['patient-management', 'schedule-management'], description: 'Choose the repo')
+                string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to checkout')
+        }
 
-    parameters {
-        choice(name: 'REPO_SELECTION', choices: ['patient-management', 'schedule-management'], description: 'Choose the repo')
-        string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to checkout')
-    }
+        environment {
+                REPO1_URL = 'https://github.com/Supriyaram/patient-management.git'
+                REPO2_URL = 'https://github.com/Supriyaram/schedule-management.git'
+                IMAGE_NAME = params.REPO_SELECTION
+        }
 
-    environment {
-        REPO1_URL = 'https://github.com/Supriyaram/patient-management.git'
-        REPO2_URL = 'https://github.com/Supriyaram/schedule-management.git'
-    }
+        stages {
+                stage('Provision EC2 & Register Agent') {
+                        steps {
+                                script {
+                                        def label = "agent-${env.BUILD_NUMBER}"
+                                        def instanceId = launchEc2Instance(label)
 
-    stages {
-        stage('Provision EC2 & Register Agent') {
-            steps {
-                script {
-                    def label = "agent-${env.BUILD_NUMBER}"
-                    def instanceId = launchEc2Instance(label)
+                                        echo "Waiting for instance ${instanceId} to be in 'running' state..."
 
-                    echo "Waiting for instance ${instanceId} to be in 'running' state..."
+                                        sh "aws ec2 wait instance-status-ok --instance-ids ${instanceId}"
 
-                    sh "aws ec2 wait instance-status-ok --instance-ids ${instanceId}"
-
-                    def privateIP = sh(script: """
+                                        def privateIP = sh(script: """
                         aws ec2 describe-instances --instance-ids ${instanceId} \
                         --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
                     """, returnStdout: true).trim()
 
-                    // Save instance ID and label to env for future stages
-                    env.SLAVE_LABEL = label
-                    env.INSTANCE_ID = instanceId
+                                        // Save instance ID and label to env for future stages
+                                        env.SLAVE_LABEL = label
+                                        env.INSTANCE_ID = instanceId
 
-                    registerEc2Agent(label, privateIP, 'jenkins-ssh-key')
+                                        registerEc2Agent(label, privateIP, 'jenkins-ssh-key')
+                                }
+                        }
                 }
-            }
+
+                stage('Check out on EC2 Agent') {
+                        agent { label "${env.SLAVE_LABEL}" }
+                        steps {
+                                script {
+                                        def repoUrl = (params.REPO_SELECTION == 'patient-management') ? env.REPO1_URL : env.REPO2_URL
+                                        echo "Cloning repo: ${repoUrl} on branch: ${params.BRANCH_NAME}"
+
+                                        // Clean workspace before new checkout
+                                        deleteDir()
+                                        //ensures app-repo is checking out inside 'app' dir
+                                        dir('app'){
+                                                checkout([
+                                                        $class           : 'GitSCM',
+                                                        branches         : [[name: "*/${params.BRANCH_NAME}"]],
+                                                        userRemoteConfigs: [[url: repoUrl]]
+                                                ])
+                                                stash name: 'app-code'
+                                        }
+                                }
+                        }
+                }
+
+                stage('Build & Test') {
+                        agent { label "${env.SLAVE_LABEL}" }
+                        steps {
+                                script {
+                                        //same app-repo must be used here since its different stage, workspace from previous will be refreshed
+                                        dir('app'){
+                                                unstash 'app-code'
+                                                def mvnHome = tool name: 'Maven3', type: 'maven'
+                                                withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                                                        sh 'mvn clean verify'
+                                                }
+                                        }
+                                }
+                        }
+                }
+                stage('Docker Build') {
+                        steps {
+                                sh ' docker build -t $IMAGE_NAME .'
+                        }
+                }
         }
 
         stage('Check out on EC2 Agent') {
@@ -87,7 +131,7 @@ pipeline {
             }
         }
     }
-}
+
 
 // This function should ideally live in the shared library, not in the Jenkinsfile
     def launchEc2Instance(String label) {
@@ -100,10 +144,11 @@ pipeline {
         --output text
     """
         return sh(script: command, returnStdout: true).trim()
-    }
-    def terminateEc2Instance(String instanceId) {
+
+}
+def terminateEc2Instance(String instanceId) {
         sh """
             aws ec2 terminate-instances --instance-ids ${instanceId}
         """
-    }
+}
 
