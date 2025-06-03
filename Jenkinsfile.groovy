d1ed70132d1a6697ae7e2d6026c84bcfde2abd26
Supriya R <supriyaram3498@gmail.com>
2025-06-03 17:14:49 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -58,8 +58,10 @@ pipeline {
             agent { label "${env.SLAVE_LABEL}" }
             steps {
                 script {
-                    sh 'which mvn && mvn -version'
-                }
+                    def mvnHome = tool name: 'Maven 3', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                    sh 'mvn clean verify'
+                 }
             }
         }
     }
