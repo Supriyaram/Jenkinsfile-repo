0c4aef7e2956518d34b9f09f12b2cfa8df19900d
Supriya <darshan01075@gail.com>
2025-06-04 01:08:56 +0530

    added docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -76,17 +76,18 @@ pipeline {
                 stage('Install AWS CLI') {
                         steps {
                                 sh '''
-                                    if ! command -v aws &> /dev/null; then
-                                        echo "Installing AWS CLI..."
-                                        sudo apt-get update -y
-                                        sudo apt-get install -y unzip curl
-                                        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                                        unzip -q awscliv2.zip
-                                        sudo ./aws/install --update
-                                        echo "AWS CLI installed at: $(which aws)"
-                                    else
-                                        echo "AWS CLI already installed: $(which aws)"
-                                    fi
+                                        aws --version
+//                                    if ! command -v aws &> /dev/null; then
+//                                        echo "Installing AWS CLI..."
+//                                        sudo apt-get update -y
+//                                        sudo apt-get install -y unzip curl
+//                                        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+//                                        unzip -q awscliv2.zip
+//                                        sudo ./aws/install --update
+//                                        echo "AWS CLI installed at: $(which aws)"
+//                                    else
+//                                        echo "AWS CLI already installed: $(which aws)"
+//                                    fi
                                 '''
                         }
                 }
