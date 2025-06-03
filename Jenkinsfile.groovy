21da192f04e82ee6bf4c12cd684fbab605a12277
Supriya <darshan01075@gail.com>
2025-06-03 22:55:09 +0530

    added docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -80,9 +80,8 @@ pipeline {
                                 script{
                                         env.IMAGE_NAME = "${params.REPO_SELECTION}"+ ":latest"
                                         echo "Using image: ${env.IMAGE_NAME}"
-                                        sh "docker --version"
+                                       sh 'mvn --version'
                                         sh 'java --version'
-                                        sh 'mvn --version'
 
 //                                        sh " docker build -t ${env.IMAGE_NAME } ."
                                 }
