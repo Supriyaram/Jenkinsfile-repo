ae25952a0731fed1d1c82b9fcccfc5d31c82bd76
Supriya <darshan01075@gail.com>
2025-06-04 11:49:39 +0530

    added docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -107,6 +107,7 @@ pipeline {
                                 
                                                     aws ecr get-login-password --region us-east-1 | \
                                                     docker login --username AWS --password-stdin ${repoUrl}
+                                                     docker push ${repoUrl}
                                                 """
                                         }
                                 }
